facefe2bc3b39c714d69d7a1a769886656b35770
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2026-06-13 21:00:56 -0300

    chore: fix ts-jest deprecated globals config

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
   testMatch: ['**/*.test.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: './tsconfig.test.json',
-    },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: './tsconfig.test.json' }],
   },
 };
